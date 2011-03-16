1ce26fe344d0d805d8edf4070c8ae2a44f4819e4
yechunchun <398198@gmail.com>
2011-03-16 12:28:38 +0800

    修改缩进 数据库操作 函数save_form_element和get_form_elements_module基本完成 只差basefile上传，应为现在文件上传块正在进行（文件上传块主要用在学生上传作业） 文件上传块将确定文件目录的具体细节，完成后就可一直接将basefile放入目录了。

--- lib.php
+++ lib.php
@@ -35,7 +35,7 @@ require_once($CFG->dirroot.'/plagiarism/lib.php');
 
 
 class plagiarism_plugin_moss extends plagiarism_plugin {
-     /**
+    /**
      * hook to allow plagiarism specific information to be displayed beside a submission 
      * @param array  $linkarraycontains all relevant information for the plugin to generate a link
      * @return string
@@ -49,105 +49,96 @@ class plagiarism_plugin_moss extends plagiarism_plugin {
         //add link/information about this file to $output
         return $output;
     }
-
-
-
-    /* hook to save plagiarism specific settings on a module settings page
+    /** hook to save plagiarism specific settings on a module settings page
      * @param object $data - data from an mform submission.
-    */
-    //未完成，数据库问题
+     */
     public function save_form_elements($data) {
-		global $DB;
-		$finished = false;
-		if($data->update <> '0'){//更新作业，删除原有设置（如果有的话）
-			$cmid = $data->coursemodule;
-			$old_settings= $DB->get_records_menu('moss_assignment_settings', array('cmid'=>$cmid),'','cmid,finished');
-			$finished = ($old_settings['finished'] == '1') ? true : false;//记录是否运行过
-			$DB->delete_records('moss_assignment_settings', array('cmid'=>$cmid));//删除原记录
-		}
-		//根据checkbox状态来写数据库
-		for($times = 0; $times <= 2; $times++){	
-			$element_name = array('checkbox'=>'active'.$times,'filename'=>'filename'.$times,'language'=>'language'.$times,
-				   				  'sensitivity'=>'sensitivity'.$times,'basefile'=>'basefile'.$times);
-			
-			if(isset($data->$element_name['checkbox'])){//设置启用，写数据库
-				$newelement = new object();
-           		$newelement->cmid = $data->coursemodule;
-            	$newelement->filename = $data->$element_name['filename'];
-            	$newelement->judger = 'moss';//judger留着
-            	$newelement->language = $data->$element_name['language'];
-            	$newelement->sensitivity = $data->$element_name['sensitivity'];
-            	$newelement->basefile = $data->$element_name['basefile'];
-            	$newelement->autoruntime = '10';//
-        		$newelement->finished = '0';
-            	//判断moss是否之前有运行过，如果有则标记，当教师进入‘moss结果界面’时提醒是否从新运行moss
-        		$newelement->rerunrequired = ($finished)? '1' : '0';      
-            	$DB->insert_record('moss_assignment_settings', $newelement);
-			}
-		}
+        global $DB;
+        $finished = false;
+        if($data->update <> '0'){//更新作业，删除原有设置（如果有的话）
+            $cmid = $data->coursemodule;
+            $old_settings= $DB->get_records_menu('moss_assignment_settings', array('cmid'=>$cmid),'','id,finished');
+            foreach ($old_settings as $record){
+            	$finished = ($record == '1') ? true : false;//记录是否运行过
+            }
+            $DB->delete_records('moss_assignment_settings', array('cmid'=>$cmid));//删除原记录
+        }
+        //根据checkbox状态来写数据库
+        for($index = 0; $index <= 2; $index++){	
+            $element_name = array('checkbox'=>'active'.$index,'filename'=>'filename'.$index,'language'=>'language'.$index,
+                                  'sensitivity'=>'sensitivity'.$index,'basefile'=>'basefile'.$index);
+            if(isset($data->$element_name['checkbox'])){//设置启用，写数据库
+                $newelement = new object();
+                $newelement->cmid = $data->coursemodule;
+                $newelement->filename = $data->$element_name['filename'];
+                $newelement->judger = 'moss';//judger留着
+                $newelement->language = $data->$element_name['language'];
+                $newelement->sensitivity = $data->$element_name['sensitivity'];
+                $newelement->basefile = $data->$element_name['basefile'];
+                $newelement->autoruntime = $data->timedue;
+                $newelement->finished = '0';
+                //判断moss是否之前有运行过，如果有则标记，当教师进入‘moss结果界面’时提醒是否从新运行moss
+                $newelement->rerunrequired = ($finished)? '1' : '0';    
+                $DB->insert_record('moss_assignment_settings', $newelement);
+            }
+        }
     }
-
-
-
     /**
      * hook to add plagiarism specific settings to a module settings page
      * @param object $mform  - Moodle form
      * @param object $context - current context
      */
-   //未完成,数据库问题
     public function get_form_elements_module($mform, $context) {
-    	global $DB;
+        global $DB;
         $mform->addElement('header', 'mossdesc', get_string('moss', 'plagiarism_moss'));
         //初始化 
-		for($times = 0; $times <= 2; $times++){
-			//添加单选框
-			$mform->addElement('checkbox', 'active'.$times, get_string('activateentry', 'plagiarism_moss'));
-			//添加作业名输入框
-        	$mform->addElement('text', 'filename'.$times, get_string('filename', 'plagiarism_moss'));
-			$mform->disabledIf('filename'.$times, 'active'.$times);
-			//添加编程语言输入框
-			$choices = array('ada' => 'Ada', 'ascii' => 'ASCII', 'a8086' => 'a8086 assembly', 'c' => 'C', 
-						 	'cc' => 'C++', 'csharp' => 'C#', 'fortran' => 'FORTRAN', 'haskell' => 'Haskell', 
-						 	'java' => 'Java', 'javascript' => 'Javascript', 'lisp' => 'Lisp', 'matlab' => 'Matlab', 
-						 	'mips' => 'MIPS assembly', 'ml' => 'ML', 'modula2' => 'Modula2', 'pascal' => 'Pascal', 
-						 	'perl' => 'Perl', 'plsql' => 'PLSQL', 'prolog' => 'Prolog', 'python' => 'Python', 
-						 	'scheme' => 'Scheme', 'spice' => 'Spice', 'vhdl' => 'VHDL', 'vb' => 'Visual Basic');
-			$mform->addElement('select', 'language'.$times, get_string('language','plagiarism_moss'),$choices);
-			$mform->disabledIf('language'.$times, 'active'.$times);
-			//添加灵敏度输入框
-			$mform->addElement('text', 'sensitivity'.$times, get_string('sensitivity','plagiarism_moss'),'size = "10"');
-			$mform->addHelpButton('sensitivity'.$times, 'sensitivity', 'plagiarism_moss');
-			$mform->disabledIf('sensitivity'.$times, 'active'.$times);
-			//添加base文件选择框
-			$mform->addElement('filepicker', 'basefile'.$times, get_string('basefile','plagiarism_moss'), null, array('maxbytes' => 1024,'accepted_types' =>'*'));
-			$mform->addHelpButton('basefile'.$times, 'basefile', 'plagiarism_moss');
-			$mform->disabledIf('basefile'.$times, 'active'.$times);
-		}
-    	//检查是否为更新，如果是则使用原有数据设置默认值
-		$cmid = optional_param('update', 0, PARAM_INT); //there doesn't seem to be a way to obtain the current cm a better way - $this->_cm is not available here.
-   		$old_settings;
-   		$default_settings = array('filename'=>'*.c','language'=>'c','sensitivity'=>'50');
-		if ($cmid <> '0') {//在update界面时，读取数据库数据，注：有可能读出空，原因是新建作业时没有设置反抄袭
-			$old_settings = $DB->delete_records('moss_assignment_settings', array('cmid'=>$cmid));//返回多维数组，每个记录一行
-            print_object($old_settings);
-			die;
-		}
+        for($index = 0; $index <= 2; $index++){
+            //添加单选框
+            $mform->addElement('checkbox', 'active'.$index, get_string('activateentry', 'plagiarism_moss'));
+            //添加作业名输入框
+            $mform->addElement('text', 'filename'.$index, get_string('filename', 'plagiarism_moss'));
+            $mform->disabledIf('filename'.$index, 'active'.$index);
+            //添加编程语言输入框
+            $choices = array('ada' => 'Ada', 'ascii' => 'ASCII', 'a8086' => 'a8086 assembly', 'c' => 'C', 
+                             'cc' => 'C++', 'csharp' => 'C#', 'fortran' => 'FORTRAN', 'haskell' => 'Haskell', 
+                             'java' => 'Java', 'javascript' => 'Javascript', 'lisp' => 'Lisp', 'matlab' => 'Matlab', 
+                             'mips' => 'MIPS assembly', 'ml' => 'ML', 'modula2' => 'Modula2', 'pascal' => 'Pascal', 
+                             'perl' => 'Perl', 'plsql' => 'PLSQL', 'prolog' => 'Prolog', 'python' => 'Python', 
+                             'scheme' => 'Scheme', 'spice' => 'Spice', 'vhdl' => 'VHDL', 'vb' => 'Visual Basic');
+            $mform->addElement('select', 'language'.$index, get_string('language','plagiarism_moss'),$choices);
+            $mform->disabledIf('language'.$index, 'active'.$index);
+            //添加灵敏度输入框
+            $mform->addElement('text', 'sensitivity'.$index, get_string('sensitivity','plagiarism_moss'),'size = "10"');
+            $mform->addHelpButton('sensitivity'.$index, 'sensitivity', 'plagiarism_moss');
+            $mform->disabledIf('sensitivity'.$index, 'active'.$index);
+            //添加base文件选择框
+            $mform->addElement('filepicker', 'basefile'.$index, get_string('basefile','plagiarism_moss'), null, array('maxbytes' => 1024,'accepted_types' =>'*'));
+            $mform->addHelpButton('basefile'.$index, 'basefile', 'plagiarism_moss');
+            $mform->disabledIf('basefile'.$index, 'active'.$index);
+        }
         //设置默认值
-		for($times = 0; $times <= 2; $times++){
-			if(isset($old_settings[$times])){//如果为更新
-				$mform->setDefault('active'.$times,'');//TODO 把checkbox设置成启用
-				$mform->setDefault('filename'.$times, $old_settings[$times]['filename']);
-				$mform->setDefault('language'.$times, $old_settings[$times]['language']);
-				$mform->setDefault('sensitivity'.$times, $old_settings[$times]['sensitivity']);
-			}else{
-				$mform->setDefault('filename'.$times, $default_settings['filename']);
-				$mform->setDefault('language'.$times, $default_settings['language']);
-				$mform->setDefault('sensitivity'.$times, $default_settings['sensitivity']);	
-			}
-		}
+        $index = 0;
+        $default_settings = array('filename'=>'*.c','language'=>'c','sensitivity'=>'50');
+
+        //检查是否为更新，如果是则使用原有数据设置默认值
+        $cmid = optional_param('update', 0, PARAM_INT); //obtain the current cm a better way - $this->_cm is not available here.
+        if ($cmid <> '0') {//在update界面时，读取数据库数据，注：有可能读出空，原因是新建作业时没有设置反抄袭	
+            $old_settings = $DB->get_records('moss_assignment_settings', array('cmid'=>$cmid));
+            if(isset($old_settings))//有旧设置
+            foreach ($old_settings as $record){
+                $mform->setDefault('active'.$index,'1');//把checkbox设置成启用
+                $mform->setDefault('filename'.$index, $record->filename);
+                $mform->setDefault('language'.$index, $record->language);
+                $mform->setDefault('sensitivity'.$index++, $record->sensitivity);
+            }
+        }
+        //使用默认数据设置默认值 
+        for(; $index <= 2; $index++){
+                $mform->setDefault('filename'.$index, $default_settings['filename']);
+                $mform->setDefault('language'.$index, $default_settings['language']);
+                $mform->setDefault('sensitivity'.$index, $default_settings['sensitivity']);	
+        }
     }
-
-
     /**
      * hook to allow a disclosure to be printed notifying users what will happen with their submission
      * @param int $cmid - course module id
@@ -163,9 +154,6 @@ class plagiarism_plugin_moss extends plagiarism_plugin {
         echo format_text($plagiarismsettings['moss_student_disclosure'], FORMAT_MOODLE, $formatoptions);
         echo $OUTPUT->box_end();
     }
-
-
-
     /**
      * hook to allow status of submitted files to be updated - called on grading/report pages.
      * @param object $course - full Course object
@@ -174,9 +162,6 @@ class plagiarism_plugin_moss extends plagiarism_plugin {
     public function update_status($course, $cm) {
         //called at top of submissions/grading pages - allows printing of admin style links or updating status
     }
-
-
-
     /**
      * called by admin/cron.php 
      *
@@ -184,42 +169,50 @@ class plagiarism_plugin_moss extends plagiarism_plugin {
     public function cron() {
         //do any scheduled task stuff
     }
- 	
-    private $modify = false;
 }
 
 
 
 function event_file_uploaded($eventdata) {
+    print_object($eventdata);
+    die;
     $result = true;
-        //a file has been uploaded - submit this to the plagiarism prevention service.
+    //a file has been uploaded - submit this to the plagiarism prevention service.
     return $result;
 }
 function event_files_done($eventdata) {
+    print_object($eventdata);
+    die;
     $result = true;
-        //mainly used by assignment finalize - used if you want to handle "submit for marking" events
-        //a file has been uploaded/finalised - submit this to the plagiarism prevention service.
+    //mainly used by assignment finalize - used if you want to handle "submit for marking" events
+    //a file has been uploaded/finalised - submit this to the plagiarism prevention service.
     return $result;
 }
 
 function event_mod_created($eventdata) {
+    print_object($eventdata);
+    die;
     $result = true;
-        //a new module has been created - this is a generic event that is called for all module types
-        //make sure you check the type of module before handling if needed.
+    //a new module has been created - this is a generic event that is called for all module types
+    //make sure you check the type of module before handling if needed.
     return $result;
 }
 
 function event_mod_updated($eventdata) {
+    print_object($eventdata);
+    die;
     $result = true;
-        //a module has been updated - this is a generic event that is called for all module types
-        //make sure you check the type of module before handling if needed.
+    //a module has been updated - this is a generic event that is called for all module types
+    //make sure you check the type of module before handling if needed.
     return $result;
 }
 
 function event_mod_deleted($eventdata) {
+    print_object($eventdata);
+    die;
     $result = true;
-        //a module has been deleted - this is a generic event that is called for all module types
-        //make sure you check the type of module before handling if needed.
+    //a module has been deleted - this is a generic event that is called for all module types
+    //make sure you check the type of module before handling if needed.
     return $result;
 }
 
