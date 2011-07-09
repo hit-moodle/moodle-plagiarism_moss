ed19630d8a16b8df151586dd1f84ebd1459bb58f
Sun Zhigang <sunner@gmail.com>
2011-07-09 15:47:09 +0800

    Redesign capability

--- db/access.php
+++ db/access.php
@@ -1,64 +1,56 @@
 <?php
 $capabilities = array(
 
-    'moodle/moss:student_page'    => array(
-         'captype'        => 'write',
-         'contextlevel'   => CONTEXT_COURSE,
-         'legacy' => array(
-         'guest'          => CAP_PREVENT,
-         'student'        => CAP_ALLOW,
-         'teacher'        => CAP_ALLOW,
-         'editingteacher' => CAP_ALLOW,
-         'coursecreator'  => CAP_ALLOW,
-         'admin'          => CAP_ALLOW,
-         'manager'        => CAP_ALLOW
-        )
+    'moodle/moss:viewselfresult'    => array(
+         'captype'        => 'read',
+         'contextlevel'   => CONTEXT_MODULE,
+         'archetypes' => array(
+             'student'        => CAP_ALLOW,
+             'teacher'        => CAP_ALLOW,
+             'editingteacher' => CAP_ALLOW,
+             'manager'        => CAP_ALLOW
+         )
     ),
-    
-    'moodle/moss:statistics_page' => array(
-        'captype'      => 'read',
-        'contextlevel' => CONTEXT_MODULE,
-        'legacy' => array(
-        'guest'          => CAP_PREVENT,
-        'student'        => CAP_PREVENT,
-        'teacher'        => CAP_ALLOW,
-        'editingteacher' => CAP_ALLOW,
-        'coursecreator'  => CAP_ALLOW,
-        'admin'          => CAP_ALLOW
-        )
-    ),
-    
-    'moodle/moss:view_all_page' => array(
-        'captype'      => 'write',
-        'contextlevel' => CONTEXT_MODULE,
-        'legacy' => array(
-        'guest'          => CAP_PREVENT,
-        'student'        => CAP_PREVENT,
-        'teacher'        => CAP_ALLOW,
-        'editingteacher' => CAP_ALLOW,
-        'coursecreator'  => CAP_ALLOW,
-        'admin'          => CAP_ALLOW
-        )
-    ),
-    
-    'moodle/moss:view_code_page' => array(
-        'captype'      => 'read',
-        'contextlevel' => CONTEXT_MODULE,
-        'legacy' => array(
-        'guest'          => CAP_PREVENT,
-        'student'        => CAP_ALLOW,
-        'teacher'        => CAP_ALLOW,
-        'editingteacher' => CAP_ALLOW,
-        'coursecreator'  => CAP_ALLOW,
-        'admin'          => CAP_ALLOW
-        )
-    ),
-);
-/*
- * 
- *  $modulecontext = get_context_instance(CONTEXT_MODULE, $cmid);
-        $output = '';
 
-        //check if this is a user trying to look at their details, or a teacher with viewsimilarityscore rights.
-        if (($USER->id == $userid) || has_capability('moodle/plagiarism_turnitin:viewsimilarityscore', $modulecontext)) {
- */
+    'moodle/moss:viewunconfirmed'    => array(
+         'captype'        => 'read',
+         'contextlevel'   => CONTEXT_MODULE,
+         'archetypes' => array(
+             'student'        => CAP_ALLOW,
+             'teacher'        => CAP_ALLOW,
+             'editingteacher' => CAP_ALLOW,
+             'manager'        => CAP_ALLOW
+         )
+    ),
+
+    'moodle/moss:viewallresults'    => array(
+         'captype'        => 'read',
+         'contextlevel'   => CONTEXT_MODULE,
+         'archetypes' => array(
+             'teacher'        => CAP_ALLOW,
+             'editingteacher' => CAP_ALLOW,
+             'manager'        => CAP_ALLOW
+         )
+    ),
+
+    'moodle/moss:viewdiff'    => array(
+         'captype'        => 'read',
+         'contextlevel'   => CONTEXT_MODULE,
+         'archetypes' => array(
+             'teacher'        => CAP_ALLOW,
+             'editingteacher' => CAP_ALLOW,
+             'manager'        => CAP_ALLOW
+         )
+    ),
+
+    'moodle/moss:confirm'    => array(
+         'captype'        => 'write',
+         'contextlevel'   => CONTEXT_MODULE,
+         'archetypes' => array(
+             'teacher'        => CAP_ALLOW,
+             'editingteacher' => CAP_ALLOW,
+             'manager'        => CAP_ALLOW
+         )
+    )
+);
+
