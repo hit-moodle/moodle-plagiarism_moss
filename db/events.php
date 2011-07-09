7575b4d51937def34ebec0dc3e07f027a7b7ce45
Sun Zhigang <sunner@gmail.com>
2011-07-09 15:47:15 +0800

    Remove unused events

--- db/events.php
+++ db/events.php
@@ -10,25 +10,4 @@ $handlers = array (
         'handlerfunction'  => 'moss_event_file_uploaded',
         'schedule'         => 'cron'
     ),
-    'assessable_files_done' => array (
-        'handlerfile'      => '/plagiarism/moss/lib.php',
-        'handlerfunction'  => 'moss_event_files_done',
-        'schedule'         => 'cron'
-    ),
-    'mod_created' => array (
-        'handlerfile'      => '/plagiarism/moss/lib.php',
-        'handlerfunction'  => 'moss_event_mod_created',
-        'schedule'         => 'cron'
-    ),
-    'mod_updated' => array (
-        'handlerfile'      => '/plagiarism/moss/lib.php',
-        'handlerfunction'  => 'moss_event_mod_updated',
-        'schedule'         => 'cron'
-    ),
-    'mod_deleted' => array (
-        'handlerfile'      => '/plagiarism/moss/lib.php',
-        'handlerfunction'  => 'moss_event_mod_deleted',
-        'schedule'         => 'cron'
-    ),
-
 );
