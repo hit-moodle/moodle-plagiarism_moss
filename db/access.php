274a43ba1acc5bf67ed5ca46be5b428afa74a861
Sun Zhigang <sunner@gmail.com>
2011-07-12 10:56:33 +0800

    Add risk masks and fix coding style

--- db/access.php
+++ db/access.php
@@ -2,44 +2,48 @@
 $capabilities = array(
 
     'plagiarism/moss:viewunconfirmed'    => array(
-         'captype'        => 'read',
-         'contextlevel'   => CONTEXT_MODULE,
-         'archetypes' => array(
-             'student'        => CAP_ALLOW,
-             'teacher'        => CAP_ALLOW,
-             'editingteacher' => CAP_ALLOW,
-             'manager'        => CAP_ALLOW
-         )
+        'captype'        => 'read',
+        'contextlevel'   => CONTEXT_MODULE,
+        'archetypes' => array(
+            'student'        => CAP_ALLOW,
+            'teacher'        => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager'        => CAP_ALLOW
+        )
     ),
 
     'plagiarism/moss:viewallresults'    => array(
-         'captype'        => 'read',
-         'contextlevel'   => CONTEXT_MODULE,
-         'archetypes' => array(
-             'teacher'        => CAP_ALLOW,
-             'editingteacher' => CAP_ALLOW,
-             'manager'        => CAP_ALLOW
-         )
+        'riskbitmask' => RISK_PERSONAL,
+
+        'captype'        => 'read',
+        'contextlevel'   => CONTEXT_MODULE,
+        'archetypes' => array(
+            'teacher'        => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager'        => CAP_ALLOW
+        )
     ),
 
     'plagiarism/moss:viewdiff'    => array(
-         'captype'        => 'read',
-         'contextlevel'   => CONTEXT_MODULE,
-         'archetypes' => array(
-             'teacher'        => CAP_ALLOW,
-             'editingteacher' => CAP_ALLOW,
-             'manager'        => CAP_ALLOW
-         )
+        'captype'        => 'read',
+        'contextlevel'   => CONTEXT_MODULE,
+        'archetypes' => array(
+            'teacher'        => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager'        => CAP_ALLOW
+        )
     ),
 
     'plagiarism/moss:confirm'    => array(
-         'captype'        => 'write',
-         'contextlevel'   => CONTEXT_MODULE,
-         'archetypes' => array(
-             'teacher'        => CAP_ALLOW,
-             'editingteacher' => CAP_ALLOW,
-             'manager'        => CAP_ALLOW
-         )
+        'riskbitmask' => RISK_XSS,
+
+        'captype'        => 'write',
+        'contextlevel'   => CONTEXT_MODULE,
+        'archetypes' => array(
+            'teacher'        => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager'        => CAP_ALLOW
+        )
     )
 );
 
